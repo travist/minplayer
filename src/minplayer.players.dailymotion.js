321d208937055ba4ad00c3c90cdd9603e00b9025
Christophe Leemans <gerfaut_ado@hotmail.com>
2013-04-22 22:07:40 +0200

    Fix console errors (Player was not ready) / Fix double space tab

--- src/minplayer.players.dailymotion.js
+++ src/minplayer.players.dailymotion.js
@@ -15,11 +15,11 @@ minplayer.players = minplayer.players || {};
  */
 minplayer.players.dailymotion = function(context, options, queue) {
 
-    /** The quality of the Dailymotion stream. */
-    this.quality = 'default';
+  /** The quality of the Dailymotion stream. */
+  this.quality = 'default';
 
-    // Derive from players base.
-    minplayer.players.base.call(this, context, options, queue);
+  // Derive from players base.
+  minplayer.players.base.call(this, context, options, queue);
 };
 
 /** Derive from minplayer.players.base. */
@@ -34,11 +34,11 @@ minplayer.players.dailymotion.prototype.constructor = minplayer.players.dailymot
  */
 minplayer.players.dailymotion.prototype.construct = function() {
 
-    // Call the players.flash constructor.
-    minplayer.players.base.prototype.construct.call(this);
+  // Call the players.flash constructor.
+  minplayer.players.base.prototype.construct.call(this);
 
-    // Set the plugin name within the options.
-    this.options.pluginName = 'dailymotion';
+  // Set the plugin name within the options.
+  this.options.pluginName = 'dailymotion';
 };
 
 /**
@@ -47,23 +47,25 @@ minplayer.players.dailymotion.prototype.construct = function() {
  * @return {number} The priority of this media player.
  */
 minplayer.players.dailymotion.getPriority = function(file) {
-    return 10;
+  return 10;
 };
 
 /**
  * @see minplayer.players.base#canPlay
+ * 
+ * @param {object} file A {@link minplayer.file} object.
  * @return {boolean} If this player can play this media type.
  */
 minplayer.players.dailymotion.canPlay = function(file) {
 
-    // Check for the mimetype for dailymotion.
-    if (file.mimetype === 'video/dailymotion') {
-        return true;
-    }
+  // Check for the mimetype for dailymotion.
+  if (file.mimetype === 'video/dailymotion') {
+    return true;
+  }
 
-    // If the path is a Dailymotion path, then return true.
-    var regex = /^http(s)?\:\/\/(www\.)?(dailymotion\.com)/i;
-    return (file.path.search(regex) === 0);
+  // If the path is a Dailymotion path, then return true.
+  var regex = /^http(s)?\:\/\/(www\.)?(dailymotion\.com)/i;
+  return (file.path.search(regex) === 0);
 };
 
 /**
@@ -73,19 +75,19 @@ minplayer.players.dailymotion.canPlay = function(file) {
  * @return {string} The ID for the provided media.
  */
 minplayer.players.dailymotion.getMediaId = function(file) {
-    var regex = '^http[s]?\\:\\/\\/(www\\.)?';
-    regex += '(dailymotion\\.com\\/video/)';
-    regex += '([a-z0-9\\-]+)';
-    regex += '_*';
-    var reg = RegExp(regex, 'i');
+  var regex = '^http[s]?\\:\\/\\/(www\\.)?';
+  regex += '(dailymotion\\.com\\/video/)';
+  regex += '([a-z0-9\\-]+)';
+  regex += '_*';
+  var reg = RegExp(regex, 'i');
 
-    // Locate the media id.
-    if (file.path.search(reg) === 0) {
-        return file.path.match(reg)[3];
-    }
-    else {
-        return file.path;
-    }
+  // Locate the media id.
+  if (file.path.search(reg) === 0) {
+    return file.path.match(reg)[3];
+  }
+  else {
+    return file.path;
+  }
 };
 
 /**
@@ -96,8 +98,7 @@ minplayer.players.dailymotion.getMediaId = function(file) {
  * @param {function} callback Called when the image is retrieved.
  */
 minplayer.players.dailymotion.getImage = function(file, type, callback) {
-    type = (type == 'thumbnail') ? '1' : '0';
-    callback('http://www.dailymotion.com/thumbnail/video/' + file.id);
+  callback('http://www.dailymotion.com/thumbnail/video/' + file.id);
 };
 
 /**
@@ -107,26 +108,26 @@ minplayer.players.dailymotion.getImage = function(file, type, callback) {
  * @return {object} The mediafront node.
  */
 minplayer.players.dailymotion.parseNode = function(item) {
-     return {
-        title: node.title,
-        description: node.description,
-        mediafiles: {
-            image: {
-                'thumbnail': {
-                    path: node.thumbnail_small_url
-                },
-                'image': {
-                    path: node.thumbnail_url
-                }
-            },
-           media: {
-                'media': {
-                    player: 'dailymotion',
-                    id: node.id
-                }
-            }
+  return {
+    title: node.title,
+    description: node.description,
+    mediafiles: {
+      image: {
+        'thumbnail': {
+          path: node.thumbnail_small_url
+        },
+        'image': {
+          path: node.thumbnail_url
         }
-     };
+      },
+      media: {
+        'media': {
+          player: 'dailymotion',
+          id: node.id
+        }
+      }
+    }
+  };
 };
 
 /**
@@ -137,12 +138,12 @@ minplayer.players.dailymotion.parseNode = function(item) {
  */
 minplayer.players.dailymotion.getNode = function(file, callback) {
 
-    var url = 'https://api.dailymotion.com/video/' + file.id;
-    url += '?fields=title,id,description,thumbnail_small_url,thumbnail_url';
-    jQuery.get(url, function(data) {
-        callback(minplayer.players.dailymotion.parseNode(data.data));
-    },
-    "jsonp");
+  var url = 'https://api.dailymotion.com/video/' + file.id;
+  url += '?fields=title,id,description,thumbnail_small_url,thumbnail_url';
+  jQuery.get(url, function(data) {
+    callback(minplayer.players.dailymotion.parseNode(data.data));
+  },
+          "jsonp");
 };
 
 /**
@@ -151,11 +152,11 @@ minplayer.players.dailymotion.getNode = function(file, callback) {
  * @param {string} event The onReady event that was triggered.
  */
 minplayer.players.dailymotion.prototype.onReady = function(event) {
-    minplayer.players.base.prototype.onReady.call(this);
-    if (!this.options.autoplay) {
-        this.pause();
-    }
-    this.onLoaded();
+  minplayer.players.base.prototype.onReady.call(this);
+  if (!this.options.autoplay) {
+    this.pause();
+  }
+  this.onLoaded();
 };
 
 /**
@@ -163,7 +164,7 @@ minplayer.players.dailymotion.prototype.onReady = function(event) {
  * @return {bool} TRUE - Player is found, FALSE - otherwise.
  */
 minplayer.players.dailymotion.prototype.playerFound = function() {
-    return (this.display.find(this.mediaFile.type).length > 0);
+  return (this.display.find(this.mediaFile.type).length > 0);
 };
 
 /**
@@ -172,7 +173,7 @@ minplayer.players.dailymotion.prototype.playerFound = function() {
  * @param {string} newQuality The new quality for the change.
  */
 minplayer.players.dailymotion.prototype.onQualityChange = function(newQuality) {
-    this.quality = newQuality.data;
+  this.quality = newQuality.data;
 };
 
 /**
@@ -182,7 +183,7 @@ minplayer.players.dailymotion.prototype.onQualityChange = function(newQuality) {
  * @return {bool} If this player implements its own playLoader.
  */
 minplayer.players.dailymotion.prototype.hasPlayLoader = function(preview) {
-    return minplayer.hasTouch || !preview;
+  return minplayer.hasTouch || !preview;
 };
 
 /**
@@ -191,7 +192,7 @@ minplayer.players.dailymotion.prototype.hasPlayLoader = function(preview) {
  * @return {bool} If this player implements its own playLoader.
  */
 minplayer.players.dailymotion.prototype.hasController = function() {
-    return minplayer.isIDevice;
+  return minplayer.isIDevice;
 };
 
 /**
@@ -199,140 +200,157 @@ minplayer.players.dailymotion.prototype.hasController = function() {
  * @return {object} The media player entity.
  */
 minplayer.players.dailymotion.prototype.createPlayer = function() {
-    minplayer.players.base.prototype.createPlayer.call(this);
+  minplayer.players.base.prototype.createPlayer.call(this);
 
-    // Insert the Dailymotion iframe API player.
-    var dailymotion_script = document.location.protocol + '//api.dmcdn.net/all.js';
-    if (jQuery('script[src="' + dailymotion_script + '"]').length == 0) {
-        var tag = document.createElement('script');
-        tag.src = dailymotion_script;
-        var firstScriptTag = document.getElementsByTagName('script')[0];
-        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-    }
+  // Insert the Dailymotion iframe API player.
+  var dailymotion_script = document.location.protocol + '//api.dmcdn.net/all.js';
+  if (jQuery('script[src="' + dailymotion_script + '"]').length === 0) {
+    var tag = document.createElement('script');
+    tag.src = dailymotion_script;
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  }
 
-    // Get the player ID.
-    this.playerId = this.options.id + '-player';
+  // Get the player ID.
+  this.playerId = this.options.id + '-player';
 
-    // Poll until the Dailymotion API is ready.
-    this.poll(this.options.id + '_dailymotion', (function(player) {
-        return function() {
-            var ready = jQuery('#' + player.playerId).length > 0;
-            ready = ready && ('DM' in window);
-            ready = ready && (typeof DM.player == 'function');
-            if (ready) {
-                // Determine the origin of this script.
-                jQuery('#' + player.playerId).addClass('dailymotion-player');
+  // Poll until the Dailymotion API is ready.
+  this.poll(this.options.id + '_dailymotion', (function(player) {
+    return function() {
+      var ready = jQuery('#' + player.playerId).length > 0;
+      ready = ready && ('DM' in window);
+      ready = ready && (typeof DM.player === 'function');
+      if (ready) {
+        // Determine the origin of this script.
+        jQuery('#' + player.playerId).addClass('dailymotion-player');
 
-                var params = {};
-                params = {
-                    id: player.playerId,
-                    api: minplayer.isIDevice ? 0 : 1,
-                    wmode: 'opaque',
-                    controls: minplayer.isAndroid ? 1 : 0,
-                    related: 0,
-                    info: 0,
-                    logo: 0
-                };
-
-
-                // Create the player.
-                player.player = new DM.player(player.playerId, {
-                    video: player.mediaFile.id,
-                    height: '100%',
-                    width: '100%',
-                    frameborder: 0,
-                    params: params
-                });
-
-                player.player.addEventListener('apiready', function() { player.onReady(player) } );
-                player.player.addEventListener('ended', function() { player.onComplete(player) });
-                player.player.addEventListener('playing', function() { player.onPlaying(player) });
-                player.player.addEventListener('progress', function() { player.onWaiting(player) });
-                player.player.addEventListener('pause', function() { player.onPaused(player) });
-                player.player.addEventListener('error', function() { player.onError(player) });
-            }
-            return !ready;
+        var params = {};
+        params = {
+          id: player.playerId,
+          api: minplayer.isIDevice ? 0 : 1,
+          wmode: 'opaque',
+          controls: minplayer.isAndroid ? 1 : 0,
+          related: 0,
+          info: 0,
+          logo: 0
         };
-    })(this), 200);
 
-    // Return the player.
-    return jQuery(document.createElement('div')).attr({
-        id: this.playerId
-    });
+
+        // Create the player.
+        player.player = new DM.player(player.playerId, {
+          video: player.mediaFile.id,
+          height: '100%',
+          width: '100%',
+          frameborder: 0,
+          params: params
+        });
+
+        player.player.addEventListener('apiready', function() {
+          player.onReady(player);
+        });
+        player.player.addEventListener('ended', function() {
+          player.onComplete(player);
+        });
+        player.player.addEventListener('playing', function() {
+          player.onPlaying(player);
+        });
+        player.player.addEventListener('progress', function() {
+          player.onWaiting(player);
+        });
+        player.player.addEventListener('pause', function() {
+          player.onPaused(player);
+        });
+        player.player.addEventListener('error', function() {
+          player.onError(player);
+        });
+      }
+      return !ready;
+    };
+  })(this), 200);
+
+  // Return the player.
+  return jQuery(document.createElement('div')).attr({
+    id: this.playerId
+  });
 };
 
 /**
  * @see minplayer.players.base#load
  */
 minplayer.players.dailymotion.prototype.load = function(file, callback) {
-    minplayer.players.base.prototype.load.call(this, file, function() {
-        this.player.load(file.id);
-        if (callback) {
-            callback.call(this);
-        }
-    });
+  minplayer.players.base.prototype.load.call(this, file, function() {
+    this.player.load(file.id);
+    if (callback) {
+      callback.call(this);
+    }
+  });
 };
 
 /**
  * @see minplayer.players.base#play
  */
 minplayer.players.dailymotion.prototype.play = function(callback) {
-    minplayer.players.base.prototype.play.call(this, function() {
-        //this.onWaiting();
-        this.player.play();
-        if (callback) {
-            callback.call(this);
-        }
-    });
+  minplayer.players.base.prototype.play.call(this, function() {
+    this.onWaiting();
+    this.player.play();
+    if (callback) {
+      callback.call(this);
+    }
+  });
 };
 
 /**
  * @see minplayer.players.base#pause
  */
 minplayer.players.dailymotion.prototype.pause = function(callback) {
-    minplayer.players.base.prototype.pause.call(this, function() {
-        this.player.pause();
-        if (callback) {
-            callback.call(this);
-        }
-    });
+  minplayer.players.base.prototype.pause.call(this, function() {
+    if (this.loaded) {
+      this.player.pause();
+      if (callback) {
+        callback.call(this);
+      }
+    }
+  });
 };
 
 /**
  * @see minplayer.players.base#stop
  */
 minplayer.players.dailymotion.prototype.stop = function(callback) {
-    minplayer.players.base.prototype.stop.call(this, function() {
-        this.player.pause();
-        if (callback) {
-            callback.call(this);
-        }
-    });
+  minplayer.players.base.prototype.stop.call(this, function() {
+    this.player.pause();
+    if (callback) {
+      callback.call(this);
+    }
+  });
 };
 
 /**
  * @see minplayer.players.base#seek
  */
 minplayer.players.dailymotion.prototype.seek = function(pos, callback) {
-    minplayer.players.base.prototype.seek.call(this, pos, function() {
-        this.onWaiting();
-        this.player.seek(pos);
-        if (callback) {
-            callback.call(this);
-        }
-    });
+  minplayer.players.base.prototype.seek.call(this, pos, function() {
+    this.onWaiting();
+    this.player.seek(pos);
+    if (callback) {
+      callback.call(this);
+    }
+  });
 };
 
 /**
  * @see minplayer.players.base#setVolume
  */
 minplayer.players.dailymotion.prototype.setVolume = function(vol, callback) {
-    minplayer.players.base.prototype.setVolume.call(this, vol, function() {
-        this.player.setVolume(vol);
-        if (callback != undefined) {
-            callback.call(this);
-        }
-    });
+  minplayer.players.base.prototype.setVolume.call(this, vol, function() {
+    if (this.loaded) {
+      this.player.setVolume(vol);
+      if (callback !== undefined) {
+        callback.call(this);
+      }
+    }
+  });
+
 };
 
 /**
@@ -351,24 +369,24 @@ minplayer.players.dailymotion.prototype.getValue = function(getter, callback) {
  * @see minplayer.players.base#getVolume
  */
 minplayer.players.dailymotion.prototype.getVolume = function(callback) {
-    this.getValue('volume', callback);
+  this.getValue('volume', callback);
 };
 
 /**
  * @see minplayer.players.base#getDuration.
  */
 minplayer.players.dailymotion.prototype.getDuration = function(callback) {
-    if (this.options.duration && callback != undefined) {
-        callback(this.options.duration);
-    }
-    else {
-        this.getValue('duration', callback);
-    }
+  if (this.options.duration && callback !== undefined) {
+    callback(this.options.duration);
+  }
+  else {
+    this.getValue('duration', callback);
+  }
 };
 
 /**
  * @see minplayer.players.base#getCurrentTime
  */
 minplayer.players.dailymotion.prototype.getCurrentTime = function(callback) {
-    this.getValue('currentTime', callback);
+  this.getValue('currentTime', callback);
 };
